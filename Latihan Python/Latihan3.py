9d37b0d48df78aee65d31be935e7861004825eeb
HerlanWibowo <106060694+HerlanWibowo@users.noreply.github.com>
2022-10-31 09:01:50 +0700

    Delete Latihan Python directory

--- Latihan Python/Latihan3.py
+++
@@ -1,8 +0,0 @@
-#Masukkan tinggi diamod
-tinggi= int(input("Masukkan tingginya :"))
-
-#lakukan perulangan agar menjadi diamond
-for i in range(1, tinggi, 2):
-    print(" "*(tinggi//2-i//2), "*"*i)
-for i in range(tinggi, 0, -2):
-    print(" "*(tinggi//2-i//2), "*"*i)
